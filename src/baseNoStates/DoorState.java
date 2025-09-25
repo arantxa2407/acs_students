4367427fc6500986e72e48b05155177fabbcea8b
Sergi Trilla <sergitrillamora@gmail.com>
2025-09-25 16:04:34 +0200

    Fix constructor of DoorState

--- src/baseNoStates/DoorState.java
+++ src/baseNoStates/DoorState.java
@@ -1,11 +1,12 @@
 package baseNoStates;
 
-public abstract class DoorState extends Door{
+public abstract class DoorState{
 
   protected Door door;
   protected String name;
 
   public DoorState(Door door){
-    this.door =  door;
+    this.door = door;
+    this.name = door.getId();
   }
 }
